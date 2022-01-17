fc63f60842d23c3cbd6647db54de0c01875d2f30
JoGebert <johannes.gebert@hlrs.de>
2022-01-17 18:46:19 +0100

    Refactoring

--- central_src/f-src/include_f90/revision_meta.f90
+++ central_src/f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'CT-Binarization'
 CHARACTER(LEN=scl), PARAMETER :: revision = 'V1.0.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = '2c62b308ffed6c79ab87090db448fceaaee15507'
+CHARACTER(LEN=scl), PARAMETER :: hash = 'c1509bc58bb25e48df4b19a122ca9497d34d84fe'
